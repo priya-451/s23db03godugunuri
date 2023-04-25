0df78464a3ba65aa5cf9f134c9ff62ffd6999078
priya-451 <S560603@nwmissouri.edu>
2023-04-25 10:24:10 -0500

    data added

--- models/customer.js
+++ models/customer.js
@@ -1,8 +1,19 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const customerSchema = mongoose.Schema({
-Cust_Name: String,
-Mail_Id: String,
-Cust_Age: Number
-})
-module.exports = mongoose.model("Customer",
-customerSchema)
+  Cust_Name: String,
+  Mail_Id: String,
+  Cust_Age: {
+    type: Number,
+    min: 18,
+    max: 120,
+    validate: {
+      validator: function(v) {
+        return Number.isInteger(v);
+      },
+      message: props => `${props.value} is not an integer!`
+    }
+  }
+});
+
+module.exports = mongoose.model("Customer", customerSchema);
--- routes/Customer.js
+++ routes/Customer.js
@@ -1,4 +1,14 @@
 var express = require('express');
+// A little function to check if we have an authorized user and continue on
+// or redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+      return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+  }
+  
 const customer_controlers= require('../controllers/customer');
 var router = express.Router();
 /* GET customer */
@@ -8,8 +18,11 @@ module.exports = router;
 router.get('/detail', customer_controlers.Customer_view_one_Page);
 /* GET create costume page */
 router.get('/create', customer_controlers.customer_create_Page);
-/* GET create update page */
-router.get('/update', customer_controlers.customer_update_Page);
+/* GET update costume page */
+router.get('/update', secured,
+customer_controlers.customer_update_Page);
 /* GET delete customer page */
 router.get('/delete', customer_controlers.customer_delete_Page);
 router.get('/sample', customer_controlers.sample);
+
+
