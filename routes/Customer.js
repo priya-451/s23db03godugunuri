a4aff9158bbc268ddfeda5b5af1c2fcbe1e9ecf3
priya-451 <S560603@nwmissouri.edu>
2023-04-27 21:42:42 -0500

    data added

--- routes/Customer.js
+++ routes/Customer.js
@@ -1,28 +1,36 @@
 var express = require('express');
-// A little function to check if we have an authorized user and continue on
-// or redirect to login.
-const secured = (req, res, next) => {
-    if (req.user) {
-      return next();
-    }
-    req.session.returnTo = req.originalUrl;
-    res.redirect("/login");
-  }
-  
-const customer_controlers= require('../controllers/customer');
+const customer_controlers = require('../controllers/customer');
+
 var router = express.Router();
+
 /* GET customer */
-router.get('/', customer_controlers.customer_view_all_Page );
-module.exports = router;
+router.get('/', customer_controlers.customer_view_all_Page);
+
+/* Get home page. */
+router.get('/', function(req, res, next) {
+  res.render('customer', { title: 'Search Result' });
+});
+
 /* GET detail customer page */
 router.get('/detail', customer_controlers.Customer_view_one_Page);
+
 /* GET create costume page */
 router.get('/create', customer_controlers.customer_create_Page);
+
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect('/login');
+};
+
 /* GET update costume page */
-router.get('/update', secured,
-customer_controlers.customer_update_Page);
+router.get('/update', secured, customer_controlers.customer_update_Page);
+
 /* GET delete customer page */
 router.get('/delete', customer_controlers.customer_delete_Page);
+
 router.get('/sample', customer_controlers.sample);
 
-
+module.exports = router;
